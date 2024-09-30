3ad359bcac958e7d292f7d33463cfaff6148ce78
iqbalwag <achiqbalp@gmail.com>
2024-09-30 17:28:48 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,13 @@ const plugin = require('tailwindcss/plugin');
 const withMT = require("@material-tailwind/html/utils/withMT");
 
 module.exports = withMT({
-  content: ["./Src/**/*.{html,js}", './src/index.html',],
+  content: ["./src/**/*.{html,js}", './src/index.html',],
   theme: {
     extend: {
       colors: {
         primary: {
           50: "#405D72",
-          100: "#758694",
-
+          100: "#758694"
         },
         neutral: {
           1: "#F2F2F2",
